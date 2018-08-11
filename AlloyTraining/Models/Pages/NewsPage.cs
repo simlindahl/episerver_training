bf86e0265ab9983b20b0841e8328b46f28c2c721
Simon Lindahl <simon.a.lindahl@gmail.com>
2018-08-11 12:11:02 +0200

    Added a news page listing and cooler navigation bar.

---
+++ AlloyTraining/Models/Pages/NewsPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AlloyTraining.Models.Blocks;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "NewsPage", GUID = "841fa6a9-aae4-429d-b7cb-2292bd3bc153", Description = "")]
+    public class NewsPage : StandardPage
+    {
+        [Display(
+            Name = "MainListing",
+            GroupName = SystemTabNames.Content,
+            Order = 315)]
+        public virtual ListingBlock MainListing { get; set; }
+    }
+}
